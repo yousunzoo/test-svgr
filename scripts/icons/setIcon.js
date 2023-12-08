59c21739d131ca3f8150a8eb009a427249ef25b3
yousunzoo <dpfvmffjqm@gmail.com>
2023-12-09 02:38:08 +0900

    refactor: size props 특정 값으로 제어

--- scripts/icons/setIcon.js
+++ scripts/icons/setIcon.js
@@ -6,19 +6,19 @@ const assetPath = path.resolve(__dirname, "../../public/static/icon");
 // Storybook을 사용하게 된다면 다음과 같이 작성할 수 있어요!
 const storyDist = path.resolve(
   __dirname,
-  "../../src/lib/common/components/Icon/Icon.stories.tsx",
+  "../../src/lib/common/components/Icon/Icon.stories.tsx"
 );
 
 // 실제로 우리가 사용할 아이콘 컴포넌트 경로
 const iconComponentDist = path.resolve(
   __dirname,
-  "../../src/lib/common/components/Icon/Icon.tsx",
+  "../../src/lib/common/components/Icon/index.tsx"
 );
 
 // 가져올 아이콘 폴더 경로
 const images = fs
   .readdirSync(assetPath)
-  .filter(file => new RegExp(`.\/|.svg`).test(file));
+  .filter((file) => new RegExp(`.\/|.svg`).test(file));
 
 const ext = /\.\w+/gi;
 const snake = /\_(.)/gi;
@@ -26,11 +26,11 @@ const space = /\s(.)/gi;
 
 // svg 파일명을 기반으로 다음과 같이 컴포넌트 명을 작성할 수 있어요!
 // 팀 내 아이콘 컴포넌트 컨벤션에 따라 커스터마이징이 가능합니다.
-const rename = name => {
+const rename = (name) => {
   return name
     .replace(ext, "")
-    .replace(space, i => i[1].toUpperCase())
-    .replace(snake, i => i[1].toUpperCase())
+    .replace(space, (i) => i[1].toUpperCase())
+    .replace(snake, (i) => i[1].toUpperCase())
     .split("")
     .map((char, idx) => (!idx ? char.toUpperCase() : char))
     .join("");
@@ -45,7 +45,7 @@ export type IconsProps = (typeof Icons)[IconName];
 export type IconProps = Omit<IconsProps, "size" | "color"> & {
   name: IconName;
   color?: string;
-  size?: number;
+  size?: 16 | 20 | 24 | 32;
   onClick?: (...args: any) => void;
   className?: string;
   [key: string]: any
@@ -60,7 +60,7 @@ export const Icon = (props: IconProps) => {
     <Component
       width={size}
       height={size}
-      color={color ?? "var(--simentic-on-surface, #121416)"}
+      color={color ?? "#333"}
       {...rest}
     />
   );
